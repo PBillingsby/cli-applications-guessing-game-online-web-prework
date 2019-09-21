896b922731e2b4a9ed8e87950898f95d7292cda0
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-21 16:37:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   rand_num = rand(1..6)
   puts "Pick a number between 1 and 6"
   guess = gets.chomp
-  if guess === rand_num
+  if guess === rand_num.to_s
     puts "You guessed the right number!"
   elsif guess == "exit"
     puts "Goodbye!"
